781b3ce3cc2a2fac2e5a46e3573ff33742f332fc
Jac22 <jacopo.prudentino@gmail.com>
2022-12-19 10:37:29 +0100

    Aggiunta funzione di chiamata all'API

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,44 @@
 <script>
+import { store } from './store';
+import axios from 'axios';
 export default {
+  data() {
+    return {
+      store,
+    }
+  },
+  methods: {
+    getFromApi: function () {
+      let myURL = store.apiURL;
+      if (store.searchText != '') {
+        let titleSearch = store.searchText;
+        // sostituire gli spazi con + per la ricerca nell'API
+        let aux = true;
+        while (aux) {
+          if (titleSearch.includes(' ')) {
+            titleSearch = titleSearch.replace(' ', '+')
+          }
+          else {
+            aux = false;
+          }
+        }
 
+        myURL += titleSearch;
+        axios
+          .get(myURL)
+          .then(res => {
+            this.store.filmList = res.results;
+            console.log('ok');
+          })
+          .catch(err => {
+            console.log('Errori: ', err);
+          })
+      }
+    }
+  },
+  mounted() {
+    this.getFromApi();
+  }
 }
 </script>
 
