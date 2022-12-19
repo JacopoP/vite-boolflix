61a5bae6d0658f10e031e978781359102f877bba
Jac22 <jacopo.prudentino@gmail.com>
2022-12-19 22:54:03 +0100

    Importata fontawesome e le due icone stelle

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import { createApp } from 'vue'
 import "/node_modules/flag-icons/css/flag-icons.min.css";
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+import { faStar as faRStar } from '@fortawesome/free-regular-svg-icons';
+library.add(faStar, faRStar);
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App)
+    .component('font-awesome-icon', FontAwesomeIcon)
+    .mount('#app')
